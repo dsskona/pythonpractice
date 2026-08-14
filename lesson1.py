b674c886c8ad78b55e6dcdd7d49cf89c0f98ea48
dsskona <dhanya.kona@gmail.com>
2026-08-14 23:18:25 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+#print your name using * symbol and each time it should be on next line
+print(("Dhanya Kona" + "\n") * 10)
+
+Name = "Dhanya Kona"
+age1 = 18
+age2 = 25
+print("Actual value:", age2)
+age2 = 20
+age1 = age2
+print("Changed value:", age2)
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+# take diameter of a circle from user and print its area
+diameter = float(input("Enter the diameter of the circle:"))
+area = 3.14 * diameter ** 2 / 4
+print("Area of the circle is:", area)
