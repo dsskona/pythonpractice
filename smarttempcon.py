834610266299b9e02906ab567a87d35504434458
dsskona <dhanya.kona@gmail.com>
2026-08-15 19:04:47 +0530

    Add files via upload
    Smart Temperature Converter

---
+++ smarttempcon.py
@@ -0,0 +1,7 @@
+#Smart Temperature Converter
+C = float(input("Enter temperature in celsius:"))
+F = (C*9/5)+32
+print("Temperature in Fahrenheit is:", F)
+K = C+273.15
+print("Temperature in Kelvin is:", K )
+print("These are the required conversions")
