cc1f7ebdb0817ec8452c6e7d46bb57fba18bc049
dsskona <dhanya.kona@gmail.com>
2026-08-15 19:06:22 +0530

    Add files via upload

---
+++ stringOperation.py
@@ -0,0 +1,8 @@
+food = input("Enter your favourite food:")
+mid = len(food)//2
+output1 = food[mid-1:mid+2]
+print("Middle three characters of your favourite food is:", output1)
+
+output2 = food[-2:]
+print("Last two characters of your favourite food is:", output2)
+
