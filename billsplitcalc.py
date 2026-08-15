3a88c05c0506613c646f628aceaf2672ae37f904
dsskona <dhanya.kona@gmail.com>
2026-08-15 19:05:31 +0530

    Add files via upload
    Bill Split Calculator

---
+++ billsplitcalc.py
@@ -0,0 +1,7 @@
+# Bill Split Calculator
+amount = float(input("Enter the total amount: "))
+friends = int(input("Enter the number of friends:"))
+amount_split = amount/friends
+print("Each friend should pay:", amount_split)
+print(type(amount_split))
+
