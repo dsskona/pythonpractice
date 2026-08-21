528b7c1f90905d6ab851a09eb4a4a4fcec77e7b6
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:38:38 +0530

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,22 @@
+marks = 85
+if(marks >= 90):
+    print("A")
+elif(marks >= 80):
+    print("B")
+
+age = 25
+if (age>=18):
+    print("You are eligible to vote")
+else:
+    print("Not eligible to vote")    
+    print("My code ends here")
+
+
+#take input and print positive if no. is greater than)....
+num = int(input("Enter a number:"))
+if num > 0: 
+    print("Positive number")
+elif num ==0:
+    print("Zero")
+else:
+    print("Negative number")   
