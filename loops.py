907a4d724da664ee8fcd0235c1858788d9693e90
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:46:50 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,23 @@
+#while loop
+num = 1
+while num<=2:
+    print("Dhanya Kona")
+    num = num+1
+
+#practice ques: print nos. 1 to 10
+i = 1
+while (i<=10):
+    print(i)
+    i+=1
+
+print("Q1 complete")    
+
+
+#10 to 1
+j = 10
+while j>=1:
+    print(j)
+    j-=1
+print("Q2 complete")
+
+
