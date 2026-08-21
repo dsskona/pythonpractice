53d3e4e8122d03e386a0bc95ad251db6f84be534
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:44:45 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,7 @@
+#unordered so no indexing, unique:no duplicacy
+food = {"Paneer", "Chole Bhature", "Sandwich", "Golgappe", "Paneer"}
+print(food)
+
+food.add("Kunafa")
+food.remove("Sandwich")
+print(food)
