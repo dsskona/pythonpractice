b6b9666614aabe2ac2b85b3c36d68d2b6ff4e575
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:38:00 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,17 @@
+str = "dhanya"
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.find("ya"))
+print(str.replace("dhanya", "dhanu"))
+print(str.count("a"))
+
+#formatting strings
+name = "Dhanya Kona"
+age = 18
+print(f"My name is {name} and my age is {age}")
+
+
+#escape sequences
+print("hello\nworld")
+print("hello\tworld")
