1f965e45d7b9b16df851610966ab84eeed6d4a3e
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:45:24 +0530

    Add files via upload

---
+++ Setprac2.py
@@ -0,0 +1,5 @@
+programmingList = ["Python", "Java", "C++", "Python", "Java", "C"]
+
+#convert it into list
+programmingSet = set(programmingList)
+print("Divya knows", len(programmingSet), "languages")
