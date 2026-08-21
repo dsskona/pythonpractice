86b2b58856f78f58310f2a139e0eb1cd4e65ee59
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:42:25 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,17 @@
+foodList = ["Cake", "Mango", "Pizza"]
+for item in foodList:
+    print(item)
+
+
+cartoonTuple = ("SpongeBob", "Pink Panther", "Teletubbies")
+for item in cartoonTuple:
+    print(item)
+
+
+#range 
+for i in range(1,6):
+    print("Iteration:", i)
+
+#range(start, stop, step)
+for item in range(1, 8, 2):
+ print(item)   
