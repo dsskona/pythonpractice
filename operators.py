5711370eff93500576e8f68eddbec00383b79758
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:44:03 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,31 @@
+#arithmetic operators
+x = 20
+y = 10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+
+#comparison operators
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#logical operators
+print(x>y and x<y)
+print(x>y or x<y)
+print(not(x>y))
+
+
+#assignment operator
+a = 2
+b = 3
+a+=6
+print(a)
