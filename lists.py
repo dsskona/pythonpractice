78da40b74f8db6da21c0db1bb66495dc6aba9602
dsskona <dhanya.kona@gmail.com>
2026-08-21 20:45:49 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+#mutable
+#can store multiple datatypes
+food = ["Waffles", "Pizza", "Chicken Lollipop", "Ice Cream"]
+print(len(food))
+print("First value of the list is:", food[0])
+
+
+#methods in list
+food[2] = "Soya Chaap"
+print(food)
+print(food[1:3])
